b5e6e7f0eb55eb5e1f1f696e28789949914722b3
JeffAllison <itsjallison@gmail.com>
2022-05-02 12:20:12 -0700

    added state to app.js

--- src/SloganForm.js
+++ src/SloganForm.js
@@ -1,12 +1,12 @@
 export default function SloganForm(props) {
-  // React forms are a pain! 
+  // React forms are a pain!
   // Track the sloganInput form state with a useState hook
-    
+
   function handleSubmit(e) {
     e.preventDefault();
 
-    // This component takes in a prop called setSlogans, which is a function that takes in an array of slogans. 
-    // Also, this component also takes in a prop called slogans, which is an arry of strings. //// On submit call the setSlogans state with a new array that is a copy of the old array with the new slogan immutably added to the end 
+    // This component takes in a prop called setSlogans, which is a function that takes in an array of slogans.
+    // Also, this component also takes in a prop called slogans, which is an array of strings. //// On submit call the setSlogans state with a new array that is a copy of the old array with the new slogan immutably added to the end
 
     // set the sloganInput form state to an empty string to reset the form
   }
